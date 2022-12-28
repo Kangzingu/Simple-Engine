f2323d3bcbd469ad2eac2684ff1ba210c054c6c9
Kangzingu <kanrhaehfdl1@naver.com>
2022-12-29 00:24:47 +0900

    Tutorial 14 진행 중
    Sound 클래스 작성 중

---
+++ Simple Engine/Simple Engine/soundclass.cpp
@@ -0,0 +1,233 @@
+#include "soundclass.h"
+
+SoundClass::SoundClass()
+{
+	m_DirectSound = 0;
+	m_primaryBuffer = 0;
+	m_secondaryBuffer1 = 0;
+}
+
+SoundClass::SoundClass(const SoundClass& other)
+{
+
+}
+
+SoundClass::~SoundClass()
+{
+
+}
+
+bool SoundClass::Initialize(HWND hwnd)
+{
+	bool result;
+
+	result = InitializeDirectSound(hwnd);
+	if (!result)
+	{
+		return false;
+	}
+
+	char waveFilename[128] = "../Resources/Audios/SampleSound.wav";
+	result = LoadWaveFile(waveFilename, &m_secondaryBuffer1);
+	if (!result)
+	{
+		return false;
+	}
+
+	result = PlayWaveFile();
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void SoundClass::Shutdown()
+{
+	ShutdownWaveFile(&m_secondaryBuffer1);
+
+	ShutdownDirectSound();
+
+	return;
+}
+
+bool SoundClass::InitializeDirectSound(HWND hwnd)
+{
+	HRESULT result;
+	DSBUFFERDESC bufferDesc;
+	WAVEFORMATEX waveFormat;
+
+	result = DirectSoundCreate8(NULL, &m_DirectSound, NULL);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	result = m_DirectSound->SetCooperativeLevel(hwnd, DSSCL_PRIORITY);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	bufferDesc.dwSize = sizeof(DSBUFFERDESC);
+	bufferDesc.dwFlags = DSBCAPS_PRIMARYBUFFER | DSBCAPS_CTRLVOLUME;
+	bufferDesc.dwBufferBytes = 0;
+	bufferDesc.dwReserved = 0;
+	bufferDesc.lpwfxFormat = NULL;
+	bufferDesc.guid3DAlgorithm = GUID_NULL;
+
+	result = m_DirectSound->CreateSoundBuffer(&bufferDesc, &m_primaryBuffer, NULL);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	waveFormat.wFormatTag = WAVE_FORMAT_PCM;
+	waveFormat.nSamplesPerSec = 44100;
+	waveFormat.wBitsPerSample = 16;
+	waveFormat.nChannels = 2;
+	waveFormat.nBlockAlign = (waveFormat.wBitsPerSample / 8) * waveFormat.nChannels;
+	waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;
+	waveFormat.cbSize = 0;
+
+	result = m_primaryBuffer->SetFormat(&waveFormat);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void SoundClass::ShutdownDirectSound()
+{
+	if (m_primaryBuffer)
+	{
+		m_primaryBuffer->Release();
+		m_primaryBuffer = 0;
+	}
+
+	if (m_DirectSound)
+	{
+		m_DirectSound->Release();
+		m_DirectSound = 0;
+	}
+
+	return;
+}
+
+bool SoundClass::LoadWaveFile(char* filename, IDirectSoundBuffer8** secondaryBuffer)
+{
+	int error;
+	FILE* filePtr;
+	unsigned int count;
+	WaveHeaderType waveFileHeader;
+	WAVEFORMATEX waveFormat;
+	DSBUFFERDESC bufferDesc;
+	HRESULT result;
+	IDirectSoundBuffer* tempBuffer;
+	unsigned char* waveData;
+	unsigned char* bufferPtr;
+	unsigned long bufferSize;
+
+	error = fopen_s(&filePtr, filename, "rb");
+	if (error != 0)
+	{
+		return false;
+	}
+
+	count = fread(&waveFileHeader, sizeof(waveFileHeader), 1, filePtr);
+	if (count != 1)
+	{
+		return false;
+	}
+
+	if ((waveFileHeader.chunkId[0] != 'R') || (waveFileHeader.chunkId[1] != 'I') ||
+		(waveFileHeader.chunkId[2] != 'F') || (waveFileHeader.chunkId[3] != 'F'))
+	{
+		return false;
+	}
+
+	if ((waveFileHeader.format[0] != 'W') || (waveFileHeader.format[1] != 'A') ||
+		(waveFileHeader.format[2] != 'V') || (waveFileHeader.format[3] != 'E'))
+	{
+		return false;
+	}
+
+	if ((waveFileHeader.subChunkId[0] != 'f') || (waveFileHeader.subChunkId[1] != 'm') ||
+		(waveFileHeader.subChunkId[2] != 't') || (waveFileHeader.subChunkId[3] != ' '))
+	{
+		return false;
+	}
+
+	if (waveFileHeader.audioFormat != WAVE_FORMAT_PCM)
+	{
+		return false;
+	}
+
+	if (waveFileHeader.numChannels != 2)
+	{
+		return false;
+	}
+
+	if (waveFileHeader.sampleRate != 44100)
+	{
+		return false;
+	}
+
+	if (waveFileHeader.bitsPerSample != 16)
+	{
+		return false;
+	}
+
+	if ((waveFileHeader.dataChunkId[0] != 'd') || (waveFileHeader.dataChunkId[1] != 'a') ||
+		(waveFileHeader.dataChunkId[2] != 't') || (waveFileHeader.dataChunkId[3] != 'a'))
+	{
+		return false;
+	}
+
+	waveFormat.wFormatTag = WAVE_FORMAT_PCM;
+	waveFormat.nSamplesPerSec = 44100;
+	waveFormat.wBitsPerSample = 16;
+	waveFormat.nChannels = 2;
+	waveFormat.nBlockAlign = (waveFormat.wBitsPerSample / 8) * waveFormat.nChannels;
+	waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;
+	waveFormat.cbSize = 0;
+
+	bufferDesc.dwSize = sizeof(DSBUFFERDESC);
+	bufferDesc.dwFlags = DSBCAPS_CTRLVOLUME;
+	bufferDesc.dwBufferBytes = waveFileHeader.dataSize;
+	bufferDesc.dwReserved = 0;
+	bufferDesc.lpwfxFormat = &waveFormat;
+	bufferDesc.guid3DAlgorithm = GUID_NULL;
+
+	result = m_DirectSound->CreateSoundBuffer(&bufferDesc, &tempBuffer, NULL);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	result = tempBuffer->QueryInterface(IID_IDirectSoundBuffer8, (void**)&*secondaryBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	tempBuffer->Release();
+	tempBuffer = 0;
+
+	fseek(filePtr, sizeof(WaveHeaderType), SEEK_SET);
+
+	waveData = new unsigned char[waveFileHeader.dataSize];
+	if (!waveData)
+	{
+		return false;
+	}
+
+	count = fread(waveData, 1, waveFileHeader.dataSize, filePtr);
+	if (count != waveFileHeader.dataSize)
+	{
+		return false;
+	}
+}
---
+++ Simple Engine/Simple Engine/soundclass.h
@@ -0,0 +1,56 @@
+#ifndef _SOUNDCLASS_H_
+#define _SOUNDCLASS_H_
+
+#pragma comment(lib, "dsound.lib")
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "winmm.lib")
+
+#include <Windows.h>
+#include <mmsystem.h>
+#include <dsound.h>
+#include <stdio.h>
+
+class SoundClass
+{
+private:
+	struct WaveHeaderType
+	{
+		char chunkId[4];
+		unsigned long chunkSize;
+		char format[4];
+		char subChunkId[4];
+		unsigned long subChunkSize;
+		unsigned short audioFormat;
+		unsigned short numChannels;
+		unsigned long sampleRate;
+		unsigned long bytesPerSecond;
+		unsigned short blockAlign;
+		unsigned short bitsPerSample;
+		char dataChunkId[4];
+		unsigned long dataSize;
+	};
+
+public:
+	SoundClass();
+	SoundClass(const SoundClass&);
+	~SoundClass();
+
+	bool Initialize(HWND);
+	void Shutdown();
+
+private:
+	bool InitializeDirectSound(HWND);
+	void ShutdownDirectSound();
+
+	bool LoadWaveFile(char*, IDirectSoundBuffer8**);
+	void ShutdownWaveFile(IDirectSoundBuffer8**);
+
+	bool PlayWaveFile();
+
+private:
+	IDirectSound8* m_DirectSound;
+	IDirectSoundBuffer* m_primaryBuffer;
+	IDirectSoundBuffer8* m_secondaryBuffer1;
+};
+
+#endif
